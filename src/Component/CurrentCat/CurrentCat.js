1cb91b6377c16aa8151317e56760003554cf84c7
JoanneAlexia <joannepulcina@gmail.com>
2021-09-27 19:01:59 +1000

    Added loading and error windows for get request.

--- src/Component/CurrentCat/CurrentCat.js
+++ src/Component/CurrentCat/CurrentCat.js
@@ -1,14 +1,29 @@
 import "./CurrentCat.css";
 import axios from 'axios';
-import React, {useState} from "react";
+import React, {useEffect, useState, useCallback} from "react";
 
 function CurrentCat(props) {
     const [currentCatInfo, setCurrentCatInfo] = useState({id: 1, url:"https://cdn2.thecatapi.com/images/bkk.jpg"});
-    function newCatButtonClickHandler(){
-        axios.get("https://api.thecatapi.com/v1/images/search").then(response => {
-        setCurrentCatInfo({id: response.data[0].id, url:response.data[0].url});
-      })
-    };
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(""); 
+
+    const newCatButtonClickHandler = useCallback(async () => {  
+        setIsLoading(true);
+        setError(null);
+        try{
+            const response = await axios.get("https://api.thecatapi.com/v1/images/search");
+            setCurrentCatInfo({id: response.data[0].id, url:response.data[0].url});
+            setIsLoading(false)
+        }catch(error){
+            setIsLoading(false);
+            setError(error.message);
+        }
+    },[]);
+
+    useEffect(() => {
+        newCatButtonClickHandler()
+    }, [newCatButtonClickHandler]);
+
     function favouriteCatClickHandler(){
         props.setFavourite([...props.favourite, {id: currentCatInfo.id, url:currentCatInfo.url}]);
     };
@@ -17,6 +32,30 @@ function CurrentCat(props) {
         backgroundColor: props.colorScheme
     };
 
+    let catWindow;
+    if(isLoading){
+        catWindow = 
+        <div className="loading-window">
+            <div className="loading-message">
+                <i class="fas fa-paw fa-spin"></i>
+                <p>Loading...</p>
+            </div>
+        </div>
+    }else{
+        catWindow = 
+        <img src={currentCatInfo.url} alt="cat"/>
+    }
+
+    if(!isLoading && error){
+        catWindow = 
+        <div className="loading-window">
+            <div className="loading-message">
+                <i class="fas fa-paw"></i>
+                <p>Loading cat data failed. {error}</p>
+            </div>
+        </div>
+    }
+    
     return(
         <div className="currentCat">
             <div className="buttons">
@@ -25,7 +64,7 @@ function CurrentCat(props) {
                     <button style={style} onClick={favouriteCatClickHandler}><i className="far fa-grin-hearts"></i> This one's a favourite</button>
                 </div>
             </div>
-            <img src={currentCatInfo.url} alt="cat"/>
+            {catWindow}
         </div>
     );
 }
